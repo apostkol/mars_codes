6d8469b0d872d9f6b9c5b9a829de49ae4eeb9072
Apostolos Kolokotronis <kolokotronisap@gmail.com>
2024-11-21 00:01:11 +0100

    init

--- build/lib/mars_currents/time_operations/yearisLeap.py
+++
@@ -1,9 +0,0 @@
-
-def yearisLeap(year):
-
-    """Find if variable year is a leap year. Returns True or False.
-    Inputs: year: float, int or str type.
-    Returns: True if year is leap | False if year is not leap.
-    """
-
-    return float(year)%4==0
