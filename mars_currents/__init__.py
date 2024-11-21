2f4fb0d70c8eafeeaafa433a71c91e0249f59a3a
Apostolos Kolokotronis <kolokotronisap@gmail.com>
2024-11-21 01:10:07 +0100

    xx

--- mars_currents/__init__.py
+++ mars_currents/__init__.py
@@ -1,26 +1,26 @@
-from mars_currents import (
-    generic,
-    grid,
-    mag_files,
-    maps,
-    spice_codes,
-    swia_files,
-    time_operations,
-)
+from __future__ import annotations
 
-__author__ = "Apostolos Kolokotronis"
-__email__ = "apostolos.kolokotronis@irf.se"
-__copyright__ = "Copyright 2020-2024"
-__license__ = "MIT"
-__version__ = "1.0.0"
-__status__ = "Prototype"
+from mars_currents import generic
+from mars_currents import grid
+from mars_currents import mag_files
+from mars_currents import maps
+from mars_currents import spice_codes
+from mars_currents import swia_files
+from mars_currents import time_operations
+
+__author__ = 'Apostolos Kolokotronis'
+__email__ = 'apostolos.kolokotronis@irf.se'
+__copyright__ = 'Copyright 2020-2024'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__status__ = 'Prototype'
 
 __all__ = [
-    "generic",
-    "grid",
-    "mag_files",
-    "swia_files",
-    "maps",
-    "spice_codes",
-    "time_operations",
+    'generic',
+    'grid',
+    'mag_files',
+    'swia_files',
+    'maps',
+    'spice_codes',
+    'time_operations',
 ]
